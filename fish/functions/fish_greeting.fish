9faa8e61d2d4f8017350e44ea2eaa32c5a4ea851
senyasaysmeow <arsenv2005@gmail.com>
2026-07-13 11:13:09 +0200

    init

---
+++ fish/functions/fish_greeting.fish
@@ -0,0 +1,3 @@
+function fish_greeting
+    pokemon-colorscripts -r
+end
---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,23 @@
+return {
+	"folke/snacks.nvim",
+	opts = {
+		dashboard = {
+			preset = {
+				header = [[       ,dPYb,                                   ,dPYb,                                
+       IP'`Yb                                   IP'`Yb                                
+  gg   I8  8I                                   I8  8I  gg                            
+  ""   I8  8'                                   I8  8'  ""                            
+  gg   I8 dP    ,ggggg,     ggg    gg   ,ggg,   I8 dP   gg    ,ggg,,ggg,     ,gggg,gg 
+  88   I8dP    dP"  "Y8ggg d8"Yb   88bgi8" "8i  I8dP    88   ,8" "8P" "8,   dP"  "Y8I 
+  88   I8P    i8'    ,8I  dP  I8   8I  I8, ,8I  I8P     88   I8   8I   8I  i8'    ,8I 
+_,88,_,d8b,_ ,d8,   ,d8',dP   I8, ,8I  `YbadP' ,d8b,_ _,88,_,dP   8I   Yb,,d8,   ,d8b,
+8P""Y88P'"Y88P"Y8888P"  8"     "Y8P"  888P"Y8888P'"Y888P""Y88P'   8I   `Y8P"Y8888P"`Y8
+]],
+			},
+			sections = {
+				{ section = "header" },
+				{ section = "keys" },
+			},
+		},
+	},
+}
